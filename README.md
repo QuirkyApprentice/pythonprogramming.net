a437c4ba9fc6bf739e6eb8490668eaa1eb460937
QuirkyApprentice <apprenticequirky@gmail.com>
2020-05-05 22:04:39 +0200

    migrated to pyside, main window

---
+++ README.md
@@ -0,0 +1,1 @@
+In this repository I create my adaptations of pythonprogramming.net tutorial
---
+++ pyqt/main.py
@@ -0,0 +1,34 @@
+from PySide2.QtGui import QGuiApplication, QWindow, QIcon # type: ignore
+import sys
+from typing import List
+
+
+class Window(QWindow):
+    def __init__(self, height: int, width: int, title: str, visible: bool = True) -> None:
+        """Creates and configures Qt window
+        """        
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, height, width)
+        self.setTitle(title)
+        if visible:
+            self.show()
+
+class ComponentFactory:
+    def createQuitBtn(self):
+        return
+
+
+def main(args: List) -> None:
+    """
+    Arguments:
+        args {List} -- sys.argv
+    """
+    app: QGuiApplication = QGuiApplication(args)
+    window: Window = Window(600, 800, "Journal manager")
+    window.setIcon(QIcon(str("./pyqt/fish.png")))
+    sys.exit(app.exec_())
+    return
+
+
+if __name__ == "__main__":
+    main(sys.argv)
